898e6ff31d5d8e1237c612e35ade6301c0e0edd4
Stephani Rocha <gomes.stephani19@gmail.com>
2022-06-22 20:27:47 -0300

    feat: [Componente Header] - ao clicar no botão de perfil a página é redirecionada para '/profile' - Requisito 11

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Proptypes from 'prop-types';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 const Header = ({ title }) => {
   const [renderButtons, setRenderButtons] = useState(true);
   const location = useLocation();
+  const history = useHistory();
 
   useEffect(() => {
     if (location.pathname.includes('/explore')
@@ -23,7 +24,7 @@ const Header = ({ title }) => {
   return (
     <div>
       <h1 data-testid="page-title">{title}</h1>
-      <button type="button">
+      <button type="button" onClick={ () => (history.push('/profile')) }>
         <img data-testid="profile-top-btn" src={ profileIcon } alt="profile" />
       </button>
       { renderButtons && (
