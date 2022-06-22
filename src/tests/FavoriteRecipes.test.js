cefd89a4d4c0512bcfeb48ac28b99d8e13f54b8d
Stephani Rocha <gomes.stephani19@gmail.com>
2022-06-22 19:08:33 -0300

    feat: Implementa testes para as pages

---
+++ src/tests/FavoriteRecipes.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe(('Testa a página Favorite Recipes'), () => {
+  it(('Verifica se existe o título e o botão "Profile"'), () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorite-recipes');
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorite-recipes');
+
+    const titleEl = screen.getByRole('heading', { name: /Favorite Recipes/i, level: 1 });
+    const buttonProfile = screen.getByTestId('profile-top-btn');
+
+    expect(titleEl).toBeInTheDocument();
+    expect(buttonProfile).toBeInTheDocument();
+  });
+});
---
+++ src/tests/Profile.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe(('Testa a página Profile'), () => {
+  it(('Verifica se existe o título e o botão "Profile"'), () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/profile');
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/profile');
+
+    const titleEl = screen.getByRole('heading', { name: /Profile/i, level: 1 });
+    const buttonProfile = screen.getByTestId('profile-top-btn');
+
+    expect(titleEl).toBeInTheDocument();
+    expect(buttonProfile).toBeInTheDocument();
+  });
+});
