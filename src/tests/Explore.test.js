5d5d36dc7cc0dd2d9f1907aa97916ae2526c689c
angel1cad1n1z <angelicadiniz_ad@hotmail.com>
2022-06-23 17:02:38 -0300

    feat: implementa botões na tela de explorar - requisitos 67 ao 69

--- src/tests/Explore.test.js
+++ src/tests/Explore.test.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import Explore from '../pages/Explore';
 // import userEvent from '@testing-library/user-event';
-import App from '../App';
+// import App from '../App';
 import renderWithRouter from '../helper/renderWithRouter';
 
 describe(('Testa a página Explore'), () => {
   it(('Verifica se existe o título e o botão "Profile"'), () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/explore');
+    renderWithRouter(<Explore />);
+    /* history.push('/explore');
     const { location: { pathname } } = history;
-    expect(pathname).toBe('/explore');
+    expect(pathname).toBe('/explore'); */
 
     const titleEl = screen.getByRole('heading', { name: /Explore/i, level: 1 });
     const buttonProfile = screen.getByTestId('profile-top-btn');
 
     expect(titleEl).toBeInTheDocument();
     expect(buttonProfile).toBeInTheDocument();
+  });
+
+  it((
+    'Verifica se os botões "Explore Foods" e  "Explore Drinks" são renderizados'
+  ), () => {
+    renderWithRouter(<Explore />);
+    const exploreFoods = screen.getByRole('button', {
+      name: /Explore Foods/i,
+    });
+
+    const exploreDrinks = screen.getByRole('button', {
+      name: /Explore Drinks/i,
+    });
+
+    expect(exploreFoods).toBeInTheDocument();
+    expect(exploreDrinks).toBeInTheDocument();
   });
 });
