3d123d2ab47481d77af864cb73451ec1513f3123
Giuseppe <giuseppeuhlmann@gmail.com>
2022-06-23 17:46:52 -0300

    feat: adiciona mais testes para o componente searchBar

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouterAndRedux from '../helper/renderWithRouterAndRedux';
 import { mealsByFirstLetter, mealsByIngredient, mealsByName } from './mock/mockMealAPI';
+import drinksByName from './mock/mockDrinkAPI';
 
 // jest.mock();
 // global.fetch = jest.fn();
@@ -16,7 +17,7 @@ const BTN_ID = 'exec-search-btn';
 const TIMEOUT_DELAY = 1000;
 
 describe(('Testa componente SearchBar'), () => {
-  it(('Verifica se é salvo na store o resultado da API quando busca pelo nome'),
+  it(('Verifica se é salvo na store o resultado da API (meals) buscando pelo nome'),
     async () => {
       const { store } = renderWithRouterAndRedux(<App />, '/foods');
 
@@ -36,45 +37,47 @@ describe(('Testa componente SearchBar'), () => {
         expect(store.getState().mealsSlice).toEqual(mealsByName)), TIMEOUT_DELAY);
     });
 
-  it(('Verifica se é salvo na store o resultado da API quando busca pelo ingrediente'),
-    async () => {
-      const { store } = renderWithRouterAndRedux(<App />, '/foods');
+  it((`Verifica se é salvo na store o resultado da
+  API (meals) buscando pelo ingrediente`),
+  async () => {
+    const { store } = renderWithRouterAndRedux(<App />, '/foods');
 
-      rest.get('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken', (_req, res, ctx) => (
-        res(ctx.json(mealsByIngredient), ctx.delay(DELAY))
-      ));
+    rest.get('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken', (_req, res, ctx) => (
+      res(ctx.json(mealsByIngredient), ctx.delay(DELAY))
+    ));
 
-      const searchInput = screen.getByRole('searchbox');
-      const searchType = screen.getByText(/ingredient/i);
-      const searchBtn = screen.getByTestId(BTN_ID);
+    const searchInput = screen.getByRole('searchbox');
+    const searchType = screen.getByText(/ingredient/i);
+    const searchBtn = screen.getByTestId(BTN_ID);
 
-      userEvent.type(searchInput, 'chicken');
-      userEvent.click(searchType);
-      userEvent.click(searchBtn);
+    userEvent.type(searchInput, 'chicken');
+    userEvent.click(searchType);
+    userEvent.click(searchBtn);
 
-      setTimeout(() => (
-        expect(store.getState().mealsSlice).toEqual(mealsByIngredient)), TIMEOUT_DELAY);
-    });
+    setTimeout(() => (
+      expect(store.getState().mealsSlice).toEqual(mealsByIngredient)), TIMEOUT_DELAY);
+  });
 
-  it(('Verifica se é salvo na store o resultado da API quando busca pela primeira letra'),
-    async () => {
-      const { store } = renderWithRouterAndRedux(<App />, '/foods');
+  it((`Verifica se é salvo na store o resultado da
+    API (meals) buscando pela primeira letra`),
+  async () => {
+    const { store } = renderWithRouterAndRedux(<App />, '/foods');
 
-      rest.get('https://www.themealdb.com/api/json/v1/1/search.php?f=a', (_req, res, ctx) => (
-        res(ctx.json(mealsByFirstLetter), ctx.delay(DELAY))
-      ));
+    rest.get('https://www.themealdb.com/api/json/v1/1/search.php?f=a', (_req, res, ctx) => (
+      res(ctx.json(mealsByFirstLetter), ctx.delay(DELAY))
+    ));
 
-      const searchInput = screen.getByRole('searchbox');
-      const searchType = screen.getByText(/first letter/i);
-      const searchBtn = screen.getByTestId(BTN_ID);
+    const searchInput = screen.getByRole('searchbox');
+    const searchType = screen.getByText(/first letter/i);
+    const searchBtn = screen.getByTestId(BTN_ID);
 
-      userEvent.type(searchInput, 'a');
-      userEvent.click(searchType);
-      userEvent.click(searchBtn);
+    userEvent.type(searchInput, 'a');
+    userEvent.click(searchType);
+    userEvent.click(searchBtn);
 
-      setTimeout(() => (
-        expect(store.getState().mealsSlice).toEqual(mealsByFirstLetter)), TIMEOUT_DELAY);
-    });
+    setTimeout(() => (
+      expect(store.getState().mealsSlice).toEqual(mealsByFirstLetter)), TIMEOUT_DELAY);
+  });
 
   it(('Verifica se é disparado um alert caso seja buscado com mais de uma letra'),
     async () => {
@@ -91,5 +94,25 @@ describe(('Testa componente SearchBar'), () => {
       userEvent.click(searchBtn);
 
       expect(alertMock).toHaveBeenCalledTimes(1);
+    });
+
+  it(('Verifica se é salvo na store o resultado da API (drinks) buscando pelo nome'),
+    async () => {
+      const { store } = renderWithRouterAndRedux(<App />, '/drinks');
+
+      rest.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita', (_req, res, ctx) => (
+        res(ctx.json(drinksByName), ctx.delay(DELAY))
+      ));
+
+      const searchInput = screen.getByRole('searchbox');
+      const searchType = screen.getByText(/name/i);
+      const searchBtn = screen.getByTestId(BTN_ID);
+
+      userEvent.type(searchInput, 'margarita');
+      userEvent.click(searchType);
+      userEvent.click(searchBtn);
+
+      setTimeout(() => (
+        expect(store.getState().drinksSlice).toEqual(drinksByName)), TIMEOUT_DELAY);
     });
 });
