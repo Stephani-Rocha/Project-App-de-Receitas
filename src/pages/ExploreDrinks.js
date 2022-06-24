055507ea9ac1aab29e42d242396ddb2dd4cba18d
angel1cad1n1z <angelicadiniz_ad@hotmail.com>
2022-06-24 15:17:50 -0300

    feat: adiciona botões naspáginas de explorar bebidas e comidas

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -5,6 +5,20 @@ import Header from '../components/Header';
 const ExploreDrinks = () => (
   <div>
     <Header title="Explore Drinks" />
+    <div>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Surprise me!
+      </button>
+    </div>
     <Footer />
   </div>
 );
