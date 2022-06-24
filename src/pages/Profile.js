616bea14fbb9eadd8aab4aefc90860f5a64acf26
Stephani Rocha <gomes.stephani19@gmail.com>
2022-06-24 17:41:03 -0300

    Implementa a tela de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const Profile = () => (
-  <div>
-    <Header title="Profile" />
-    <Footer />
-  </div>
-);
+const Profile = () => {
+  // refatorar se possível....
+  const getUser = localStorage.getItem('user');
+  const getEmail = JSON.parse(getUser);
+
+  const history = useHistory();
+
+  const handleClickLogin = () => {
+    history.push('/');
+    localStorage.clear();
+  };
+
+  return (
+    <div>
+      <Header title="Profile" />
+      <p data-testid="profile-email">{getEmail.email}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => { history.push('/done-recipes'); } }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => { history.push('/favorite-recipes'); } }
+      >
+        Favorite Recipes
+      </button>
+      <button type="button" data-testid="profile-logout-btn" onClick={ handleClickLogin }>
+        Logout
+      </button>
+      <Footer />
+    </div>
+  );
+};
 
 export default Profile;
