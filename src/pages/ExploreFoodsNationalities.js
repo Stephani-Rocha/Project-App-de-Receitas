90c7aad6ac0dfaee8d3065b144c701c7168fd5b9
Leandro Paiva <leandroapaiva@yahoo.com>
2022-06-28 18:07:48 -0300

    main-group-3-chamadaApi para foods e nacionalidades selecionadas

--- src/pages/ExploreFoodsNationalities.js
+++ src/pages/ExploreFoodsNationalities.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
+const numberTwelve = 12;
+
 function ExploreFoodsNationalities() {
   const [nationalityArray, setNationalityArray] = useState([]);
   async function getNationality() {
     try {
-      // const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${target.value}`);
       const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
       const data = await response.json();
       setNationalityArray(data.meals);
@@ -18,15 +19,25 @@ function ExploreFoodsNationalities() {
   useEffect(() => {
     getNationality();
   }, []);
-  console.log(nationalityArray);
+
+  async function setFilterNationality({ target }) {
+    try {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${target.value}`);
+      const data = await response.json();
+      // setFoodCards(data.meals.slice(0, numberTwelve));
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
       <Header title="Explore Nationalities" />
 
       <label htmlFor="nationalityFilter">
         <select
-          // value={ nationality }
-          onChange={ ({ target }) => setFilterNationality(target.value) }
+          onChange={ setFilterNationality }
           data-testid="explore-by-nationality-dropdown"
         >
           { nationalityArray !== null && nationalityArray !== undefined
@@ -46,4 +57,5 @@ function ExploreFoodsNationalities() {
     </div>
   );
 }
+
 export default ExploreFoodsNationalities;
