7967369ad292a1ae8c5bd520c721f96a9aee6f3d
Giuseppe <giuseppeuhlmann@gmail.com>
2022-06-30 17:09:44 -0300

    feat: implementa os data-testids dos elementos

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,10 +1,65 @@
 import React from 'react';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
 
-const DoneRecipes = () => (
-  <div>
-    <Header title="Done Recipes" />
-  </div>
-);
+const DoneRecipes = () => {
+  const doneTest = [{
+    id: '1',
+    type: 'food',
+    nationality: '',
+    category: '',
+    alcoholicOrNot: '',
+    name: '',
+    image: '',
+    doneDate: '',
+    tags: ['Pasta', 'Curry'],
+  }];
+
+  return (
+    <div>
+      <Header title="Done Recipes" />
+      <div>
+        <button type="button" data-testid="filter-by-all-btn">All</button>
+        <button type="button" data-testid="filter-by-food-btn">Food</button>
+        <button type="button" data-testid="filter-by-drink-btn">Drink</button>
+      </div>
+      <div>
+        {
+          doneTest.map((recipe, index) => (
+            <>
+              <img
+                src={ recipe.image }
+                alt={ recipe.name }
+                data-testid={ `${index}-horizontal-image` }
+              />
+              <span data-testid={ `${index}-horizontal-top-text` }>
+                {recipe.category}
+              </span>
+              <h5 data-testid={ `${index}-horizontal-name` }>
+                {recipe.name}
+              </h5>
+              <span data-testid={ `${index}-horizontal-done-date` }>
+                {recipe.doneDate}
+              </span>
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-share-btn` }
+              >
+                <img src={ shareIcon } alt="share button" />
+              </button>
+              {
+                recipe.tags.map((tag, i) => (
+                  <span key={ i } data-testid={ `${index}-${tag}-horizontal-tag` }>
+                    {tag}
+                  </span>
+                ))
+              }
+            </>
+          ))
+        }
+      </div>
+    </div>
+  );
+};
 
 export default DoneRecipes;
